7ed09b26c7f45f627c5bb08806d42eed3fee717e
ushara09 <thimalushara@gmail.com>
2022-03-15 21:09:22 +0530

    completed

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { schema } = mongoose;
+const unipueValidator = require("mongoose-unique-validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+userSchema.plugin(unipueValidator, { message: "Email already in use" });
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
